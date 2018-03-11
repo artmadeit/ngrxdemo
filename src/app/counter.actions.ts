4861094d3dd366947e76cbd5c58cd22811c7db68
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2018-03-11 17:26:10 -0500

    feat: move to polymorphic reducer

--- src/app/counter.actions.ts
+++ src/app/counter.actions.ts
@@ -1,26 +1,29 @@
 import { Action } from '@ngrx/store';
 
-export enum CounterActionTypes {
-  INCREMENT = '[Counter] Increment',
-  DECREMENT = '[Counter] Decrement',
-  RESET = '[Counter] Reset'
+export interface CounterAction extends Action {
+  execute(state): number;
 }
 
-export class Increment implements Action {
-  readonly type = CounterActionTypes.INCREMENT;
+export class Increment implements CounterAction {
+  readonly type = '[Counter] Increment';
+  execute(state) {
+    return state + 1;
+  }
 }
 
-export class Decrement implements Action {
-  readonly type = CounterActionTypes.DECREMENT;
+export class Decrement implements CounterAction {
+  readonly type = '[Counter] Decrement';
+  execute(state) {
+    return state - 1;
+  }
 }
 
-export class Reset implements Action {
-  readonly type = CounterActionTypes.RESET;
+export class Reset implements CounterAction {
+  readonly type = '[Counter] Reset';
 
-  constructor(public payload: number) {}
+  constructor(public payload: number) { }
+
+  execute(state) {
+    return this.payload; // typed to number
+  }
 }
-
-export type CounterActions
-  = Increment
-  | Decrement
-  | Reset;
--- src/app/counter.reducer.ts
+++ src/app/counter.reducer.ts
@@ -1,21 +1,7 @@
-import { CounterActionTypes, CounterActions } from './counter.actions';
+import { Action } from '@ngrx/store';
+import { CounterAction } from './counter.actions';
 
-export function reducer(state: number = 0, action: CounterActions): number {
-  switch (action.type) {
-    case CounterActionTypes.INCREMENT: {
-      return state + 1;
-    }
-
-    case CounterActionTypes.DECREMENT: {
-      return state - 1;
-    }
-
-    case CounterActionTypes.RESET: {
-      return action.payload; // typed to number
-    }
-
-    default: {
-      return state;
-    }
-  }
+export function reducer(state: number = 0, action: Action): number {
+  return 'execute' in action ?
+    (action as CounterAction).execute(state) : state;
 }
